76364305417b928609d717421cdb96a6cc31327c
Donatus Ajaezu <donatusajaezu@gmail.com>
2024-07-23 15:33:04 +0100

    update

---
+++ build/lib/bash_class_example/module1.py
@@ -0,0 +1,72 @@
+class FirstNonConsecutive:
+    def __init__(self, arr):
+        assert isinstance(arr, object)
+        self.arr = arr
+
+    def find_first_non_consecutive(self):
+        for num in range(1, len(self.arr)):
+            if self.arr[num] != self.arr[num - 1] + 1:
+                return self.arr[num]
+        return None
+
+    @staticmethod
+    def find_first_non_consecutive(arr):
+        for num in range(1, len(arr)):
+            if arr[num] != arr[num - 1] + 1:
+                return arr[num]
+        return None
+
+    @classmethod
+    def find_first_non_consecutive(cls, arr):
+        for num in range(1, len(arr)):
+            if arr[num] != arr[num - 1] + 1:
+                return arr[num]
+        return None
+
+
+class FindShortString:
+    def find_short(self, string):
+        self.string = string
+        self.words = string.split()
+        self.sort = sorted(self.words, key=len)
+        if len(self.sort) == 1:
+            self.short = "".join(self.sort)
+            return len(self.short)
+        else:
+            for word in self.sort:
+                self.short = len(self.sort[0])
+                if len(word) <= self.short:
+                    self.short = word
+        return self.short
+
+
+class Strlen:
+    @staticmethod
+    def strlen(string):
+        i = 0
+        for char in string:
+            i += 1
+        return i
+
+
+class FindMaxAndMin:
+    def __init__(self, arr):
+        self.arr = arr
+        self.min_value = None
+        self.max_value = None
+        self.find_max_and_min()
+
+    def find_max_and_min(self):
+        if not self.arr:
+            # Handle empty array
+            return
+
+        self.max_value = self.arr[0]
+        self.min_value = self.arr[0]
+
+        for num in self.arr:
+            if num > self.max_value:
+                self.max_value = num
+            elif num < self.min_value:
+                self.min_value = num
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bash_class_example',
+    version='0.1.0',
+    author='Donatus Ajaezu',
+    author_email='donatusajaezu@example.com',
+    description='A test of Classes stored in Bash Class',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/stbbash/bash_class_example.git',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
